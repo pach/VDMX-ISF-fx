948dc0dce3fbeb8d33d6640b072675642f708281
Thomas Pachoud <thomas.pachoud@gmail.com>
2014-09-09 14:13:31 +0200

    gestion de l'alpha

--- 3FalseColors.fs
+++ 3FalseColors.fs
@@ -49,5 +49,7 @@ void main() {
 	vec4 color1 = mix(darkColor, mediumColor, clamp(luminance, 0., 0.5)*2.);
 	vec4 color2 = mix(mediumColor, brightColor, clamp(luminance, 0.5, 1.)*2.-1.);
 
-	gl_FragColor = mix(color1, color2, luminance);
+	vec4 outColor = mix(color1, color2, luminance);
+    outColor.a = srcPixel.a;
+    gl_FragColor = outColor;
 }
